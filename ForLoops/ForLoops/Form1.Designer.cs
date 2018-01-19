733715a5bc60727e1a47a550d43650316003ae89
Baard Aap <martinvantongerlo@gmail.com>
2018-01-19 09:46:23 +0100

    Add files via upload

---
+++ ForLoops/ForLoops/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ForLoops
+{
+    partial class timetostartcoding
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.quickmafs = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // quickmafs
+            // 
+            this.quickmafs.Location = new System.Drawing.Point(170, 100);
+            this.quickmafs.Name = "quickmafs";
+            this.quickmafs.Size = new System.Drawing.Size(158, 23);
+            this.quickmafs.TabIndex = 0;
+            this.quickmafs.Text = "Quick Maths!";
+            this.quickmafs.UseVisualStyleBackColor = true;
+            this.quickmafs.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(170, 129);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "If / Else";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(170, 158);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "voorwaardelijke statement";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(170, 187);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(158, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "For/While loop";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timetostartcoding
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 325);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.quickmafs);
+            this.Name = "timetostartcoding";
+            this.Text = "ForLoops";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button quickmafs;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ ForLoops/ForLoops/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ForLoops
+{
+    public partial class timetostartcoding : Form
+    {
+        public timetostartcoding()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Code voor Buttonclick  Quick Mafs!:)
+            string name = "Martin";
+            int x = 3; //int x met waarde van 3
+            x = x * 17; // verander waarde van x in 3x17
+            double d = Math.PI / 2;  //int met decimale , PI delen door 2
+            MessageBox.Show("Name is " + name //Toont messagebox met naam, plus waardes van variabele x en D.
+                + "\nX is " + x
+                + "\nD is " + d);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x = 5; //variabele int X met waarde 5
+            if (x == 10) // als conditie, als x gelijk is aan tien
+            {
+                MessageBox.Show("x is  10"); //Toon messagebox x is 10
+            }
+            else // else conditie, als x niet gelijk is aan 10
+            {
+                MessageBox.Show("x isn't 10"); // toon messagebox x is niet gelijk aan tien
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int Somevalue = 4; //variabalen int SomeValue met waarde 4
+            string name = "Bobo Jr."; //string name met waarde BoBo Jr.
+            if ((Somevalue == 3) && (name == "Joe")) // if statement met expressions als Somevalue gelijk is aan 3 en name gelijk aan Joe
+            {
+                MessageBox.Show("SomeValue is 3 and name is Joe");// toon message box SomeValue is 3 and name is Joe
+            }
+            MessageBox.Show("This line runs because SomeValue isn't 3 and name isn't Joe.");//anders  toon messagebox this line runs because SomeValue isn't 3 and name isn't Joe.
+        }
+      
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int count = 0; // int count met waarde 0
+            while(count < 10) ; //while loop, als count kleiner is als 10
+            {
+                count = count + 1; //verander count en voeg 1 toe
+            }
+            for int(i = 0; i < 5; i++) // for loop int i is gelijk aan nul. als i kleiner is als vijf increment i met 1
+            {
+                count = count -1 //als i < 5 - 1 op count
+            }
+
+            MessageBox.Show("The answer is " + count) // toont messagebox met antwoord is int count.
+        }
+
+
+    }
+}
