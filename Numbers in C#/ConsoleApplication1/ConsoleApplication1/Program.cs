cb70c89787ab11734610c7f9764d4a4fad608ffd
Baard Aap <martinvantongerlo@gmail.com>
2018-01-19 09:50:30 +0100

    Add files via upload

---
+++ Numbers in C#/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+
+        static void ChallengeAnswer()
+        {
+            int sum = 0;
+            for (int number = 1; number < 21; number++)
+            {
+                if (number % 3 == 0)
+                {
+                    sum = sum + number;
+                }
+            }
+            Console.WriteLine($"The sum is {sum}");
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            ChallengeAnswer();
+        }
+        
+    }
+
+}
---
+++ Numbers in C#/Numbers/Numbers/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers
+{
+    class Program
+    {
+
+        //Working with integers method.
+        static void WorkingWithIntegers()
+        {
+            int a = 18;
+            int b = 16;
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+
+        //another working with integers method.
+        static void OrderPrecedence()
+        {
+            int a = 5;
+            int b = 4;
+            int c = 2;
+            int d = a + b * c;
+            Console.WriteLine(d);
+
+            d = (a + b) * c;
+            Console.ReadLine();
+
+            d = (a + b) - 6 * c + (12 * 3) / 3 + 12;
+            Console.WriteLine(d);
+
+            int e = 7;
+            int f = 4;
+            int g = 3;
+            int h = (e + f) / g;
+
+        }
+
+        //main void method,roept WorkingWithIntegers aan.
+        static void Main(string[] args)
+        {
+            //WorkingWithIntegers();
+            OrderPrecedence();
+        }
+    
+    }
+}
